b71ac4e3e0496013a5d558bfd490b537d71452d7
subramanyam <subbuvenkat2495@gmail.com>
2023-11-06 11:05:16 +0530

    added-local-storage-conept

--- src/Simple-Todo-App/App.jsx
+++ src/Simple-Todo-App/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import swal from 'sweetalert';
 import {FaEdit, FaTrash} from 'react-icons/fa'
 import '../style/style.css'
+
+// LocalStorage concept - 2
+const getLocalStorage = () => {
+    let list = localStorage.getItem('todosList')
+    if (list) {
+        return JSON.parse(localStorage.getItem('todosList'))
+    } else {
+        return []
+    }
+}
 
 export const App = () => {
     // For user input
@@ -13,15 +23,21 @@ export const App = () => {
     }
 
     // To add todos into array
-    const [todosArray, setTodosArray] = useState([])
-    const[editBool,setEditBool] = useState(false)
+    const [todosArray, setTodosArray] = useState(getLocalStorage())
+    const [editBool, setEditBool] = useState(false)
+
+    // LocalStorage concept - 1
+    useEffect(() => {
+        localStorage.setItem('todosList', JSON.stringify(todosArray))
+    }, [todosArray])
 
     const reusableCode = () => {
-        if(inputValue && editBool ){
+        if (inputValue && editBool) {
             const result = todosArray.map((ele) => {
-                if(ele.id === editItemId){
+                if (ele.id === editItemId) {
                     return {
-                        ...ele,todo:inputValue
+                        ...ele,
+                        todo: inputValue
                     }
                 }
                 return ele
@@ -30,7 +46,7 @@ export const App = () => {
             setTodosArray(result)
             setInputValue('');
             swal('Todo Edited');
-        }else {
+        } else {
             setTodosArray([{
                 id: uuidv4(),
                 todo: inputValue
@@ -61,11 +77,11 @@ export const App = () => {
         setTodosArray(filterData)
     }
 
-    const[editItemId,setEditItemId] = useState(null)
+    const [editItemId, setEditItemId] = useState(null)
 
     // Logic to edit each todo
     const handleEditTodo = (id) => {
-        const specificItem = todosArray.find((item) => item.id === id )
+        const specificItem = todosArray.find((item) => item.id === id)
         // console.log(specificItem);
         setEditItemId(id)
         setEditBool(true)
